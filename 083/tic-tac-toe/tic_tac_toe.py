f508fe77a209893abf01e3d73aee02d631fd3c0a
Josh Symes <josh.symes@recycleye.com>
2022-06-28 20:50:13 +0100

    day 83

--- 083/tic-tac-toe/tic_tac_toe.py
+++ 083/tic-tac-toe/tic_tac_toe.py
@@ -4,42 +4,77 @@ from controller import Controller
 from utils import clear_screen
 
 
+class Screen:
+    def display(self, *args) -> None:
+        self._clear_screen()
+        for arg in args:
+            print(arg)
+
+
+    def _clear_screen(self) -> None:
+        clear_screen()
+
+
 class TicTacToe:
     def __init__(self) -> None:
         self.board = [[None, None, None] for _ in range(3)]
         self.active_cell = (0, 0)
         self.controller = Controller()
-        self.players = ["O", "X"]
-        self.current_player = self.players[0]
+        self.players = ["X", "O"]
+        self.active_player = self.players[0]
+        self.last_active_player = self.players[1]
+        self.screen = Screen()
 
 
     def play(self) -> None:
-        clear_screen()
-        print(self._render_splash_screen())
-        sleep(3)
-        clear_screen()
-        print(self._render_instructions())
-        self._wait_for_input()
-        while True:
-            clear_screen()
-            print(self._render_frame())
-            k = self.controller.get_input()
-            self._handle_input(k)
-            clear_screen()
-            print(self._render_frame())
+        self._game_start()
+        self._play_round()
+
+
+    def _play_round(self) -> None:
+        self.board = [[None, None, None] for _ in range(3)]
+        self.active_cell = (0, 0)
+        self.active_player = self.players[1 if self.last_active_player == self.players[0] else 0]
+        round_in_progress = True
+        while round_in_progress:
+            self._play_turn()
             winner = self._check_winner()
             if winner is not None:
                 self.active_cell = None
-                clear_screen()
-                print(self._render_frame())
-                print(f"Player {winner} wins!")
-                sys.exit(0)
+                self.active_player = None
+                self.screen.display(
+                    self._render_frame(),
+                    f"Player {winner} wins!"
+                )
+                round_in_progress = False
             elif self._check_stalemate():
-                clear_screen()
-                self.active_cell = None
-                print(self._render_frame())
-                print("Stalemate!")
-                sys.exit(0)
+                self.active_player = None
+                self.screen.display(
+                    self._render_frame(),
+                    "Stalemate!"
+                )
+                round_in_progress = False
+        play_again = input("play again? (y/n)")
+        if play_again == "y":
+            self._play_round()
+        else:
+            sys.exit(0)
+
+
+    def _play_turn(self) -> None:
+        self.screen.display(self._render_frame())
+        k = self.controller.get_input()
+        self._handle_input(k)
+        self.screen.display(self._render_frame())
+
+
+
+    def _game_start(self) -> None:
+        self.screen.display(self._render_splash_screen())
+        sleep(3)
+        self.screen.display(self._render_instructions())
+        self._wait_for_input()
+
 
     def _handle_input(self, input: str) -> None:
         if input == "up" or input == "down" or input == "left" or input == "right":
@@ -47,10 +82,10 @@ class TicTacToe:
         elif input == "quit":
             sys.exit(0)
         elif input == "x":
-            if self.current_player == self.players[1]:
+            if self.active_player == self.players[1]:
                 self._handle_enter_pressed()
         elif input == "o":
-            if self.current_player == self.players[0]:
+            if self.active_player == self.players[0]:
                 self._handle_enter_pressed()
         elif input == "enter":
             self._handle_enter_pressed()
@@ -69,8 +104,8 @@ class TicTacToe:
             self.active_cell = (self.active_cell[0], min(self.active_cell[1] + 1, len(self.board[0]) - 1))
 
 
-    def _render_current_player(self) -> str:
-        return f"Current player: {self.current_player}"
+    def _render_active_player(self) -> str:
+        return f"Current player: {self.active_player}"
 
 
     def _render_board(self) -> str:
@@ -134,7 +169,9 @@ class TicTacToe:
         return f"{padding_left}{symbol}{padding_right}"
 
     def _render_frame(self) -> str:
-        frame = self._render_board() + "\n" + self._render_current_player()
+        frame = self._render_board() + "\n"
+        if self.active_player is not None:
+            frame += self._render_active_player() + "\n"
         return frame
 
 
@@ -145,8 +182,10 @@ class TicTacToe:
 
     def _handle_enter_pressed(self) -> None:
         if self.board[self.active_cell[0]][self.active_cell[1]] is None:
-            self.board[self.active_cell[0]][self.active_cell[1]] = self.current_player
-            self.current_player = self.players[1 if self.current_player == self.players[0] else 0]
+            self.board[self.active_cell[0]][self.active_cell[1]] = self.active_player
+            self.last_active_player = self.active_player
+            self.active_player = self.players[1 if self.active_player == self.players[0] else 0]
+            self.active_cell = self._first_empty_cell()
 
 
     def _check_winner(self):
@@ -164,8 +203,15 @@ class TicTacToe:
 
 
     def _check_stalemate(self) -> bool:
-        for row in self.board:
-            for cell in row:
+        if self._first_empty_cell() is None:
+            return True
+        else:
+            return False
+
+
+    def _first_empty_cell(self):
+        for y, row in enumerate(self.board):
+            for x, cell in enumerate(row):
                 if cell is None:
-                    return False
-        return True
+                    return (y, x)
+        return None
